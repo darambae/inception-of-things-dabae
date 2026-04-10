5cfc22771b41d04250d8434c8e931e24acc62a04
Daram Bae <baedaram@gmail.com>
2026-04-10 14:48:38 +0200

    changed folder names and finished part 2

--- p2/setup.sh
+++ p2/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get update
-sudo -E apt-get install -y -q openssh-client curl
+sudo -E apt-get install -y -q curl
 
 curl -sfL https://get.k3s.io | sh -s - server \
   --write-kubeconfig-mode 644 \
@@ -14,16 +15,4 @@ if ! grep -q "alias k=" /home/vagrant/.bashrc; then
     echo 'complete -F __start_kubectl k' >> /home/vagrant/.bashrc
 fi
 
-mkdir -p /home/vagrant/.ssh
-if [ ! -f /home/vagrant/.ssh/id_rsa.pub ]; then
-    ssh-keygen -q -t rsa -N "" -f /home/vagrant/.ssh/id_rsa
-fi
-
-sudo chown -R vagrant:vagrant /home/vagrant/.ssh
-chmod 700 /home/vagrant/.ssh
-chmod 600 /home/vagrant/.ssh/id_rsa
-chmod 644 /home/vagrant/.ssh/id_rsa.pub
-
-cp /home/vagrant/.ssh/id_rsa.pub /vagrant/controller_id_rsa.pub
-
-sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
+kubectl apply -f /vagrant/apps.yaml
