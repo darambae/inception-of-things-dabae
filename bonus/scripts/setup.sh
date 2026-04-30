49ad6dde754d7065c1945da7bc59d5a64d3a7a0c
Daram Bae <baedaram@gmail.com>
2026-04-30 13:30:08 +0200

    fix

--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -11,14 +11,23 @@ helm install gitlab gitlab/gitlab \
   -f ../confs/values.yaml \
   --timeout 30m
 
-ARGOCD_PWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
-argocd login 127.0.0.1:9443 --username admin --password "$ARGOCD_PWD" --insecure
+# ARGOCD_PWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
+# argocd login 127.0.0.1:9443 --username admin --password "$ARGOCD_PWD" --insecure
 
 GITLAB_PWD=$(kubectl -n gitlab get secret gitlab-gitlab-initial-root-password -o jsonpath="{.data.password}" | base64 --decode)
-argocd repo add http://gitlab.127.0.0.1.nip.io:8081/root/inception-of-things-bonus.git \
-    --username root \
-    --password "$GITLAB_PWD" \
-    --insecure \
-    --server 127.0.0.1:9443
+git clone https://github.com/darambae/inception-of-things-dabae.git
+cp -r inception-of-things-dabae/bonus inception-of-things-bonus
+cd inception-of-things-bonus
+
+git init --initial-branch=main
+git remote add origin http://gitlab.127.0.0.1.nip.io:8081/root/inception-of-things-bonus.git
+git add .
+git commit -m "Initial commit"
+git push -u origin main --force
+# argocd repo add http://gitlab.127.0.0.1.nip.io:8081/root/inception-of-things-bonus.git \
+#     --username root \
+#     --password "$GITLAB_PWD" \
+#     --insecure \
+#     --server 127.0.0.1:9443
 
 # kubectl apply -f ../confs/application.yaml
