a0ea0fa24458f8e342741b5fd35aaa202a071988
brener kelly <kellybrener66@gmail.com>
2026-05-19 16:15:20 +0200

    try with image gitlab

--- bonus/scripts/connect-gitlab-argocd.sh
+++ bonus/scripts/connect-gitlab-argocd.sh
@@ -14,11 +14,8 @@ if [ ! -f ~/.ssh/id_rsa_argocd ]; then
     ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa_argocd -N ""
 fi
 
-TOOLBOX_POD=$(kubectl get pods -n gitlab -o custom-columns=NAME:.metadata.name --no-headers | grep toolbox | head -n 1)
-
-PUB_KEY=$(cat ~/.ssh/id_rsa_argocd.pub)
-
-kubectl exec -i -n gitlab "$TOOLBOX_POD" -- env PUB_KEY="$PUB_KEY" gitlab-rails runner "
+# TOOLBOX_POD=$(kubectl get pods -n gitlab -o custom-columns=NAME:.metadata.name --no-headers | grep toolbox | head -n 1)
+kubectl exec -i -n gitlab deploy/gitlab -- env PUB_KEY="$PUB_KEY" gitlab-rails runner "
 user = User.find_by_username('root')
 if user
   key = Key.find_by(title: 'argocd-ssh-key') || Key.new(title: 'argocd-ssh-key', user: user)
@@ -33,15 +30,36 @@ else
 end
 "
 
+PUB_KEY=$(cat ~/.ssh/id_rsa_argocd.pub)
+
+# kubectl exec -i -n gitlab "$TOOLBOX_POD" -- env PUB_KEY="$PUB_KEY" gitlab-rails runner "
+# user = User.find_by_username('root')
+# if user
+#   key = Key.find_by(title: 'argocd-ssh-key') || Key.new(title: 'argocd-ssh-key', user: user)
+#   key.key = ENV['PUB_KEY']
+#   if key.save
+#     puts '🚀 SSH Key Registered Successfully'
+#   else
+#     puts key.errors.full_messages
+#   end
+# else
+#   puts '❌ Root user not found'
+# end
+# "
+
 ARGOCD_PWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
 
 argocd login 127.0.0.1:9443 --username admin --password "$ARGOCD_PWD" --insecure
 
-argocd repo add git@gitlab-gitlab-shell.gitlab.svc.cluster.local:root/inception-of-things-bonus.git \
-    --ssh-private-key-path ~/.ssh/id_rsa_argocd \
-    --insecure-ignore-host-key \
-    --server 127.0.0.1:9443 || echo "⚠️ Repo already exists"
+# argocd repo add git@gitlab-gitlab-shell.gitlab.svc.cluster.local:root/inception-of-things-bonus.git \
+#     --ssh-private-key-path ~/.ssh/id_rsa_argocd \
+#     --insecure-ignore-host-key \
+#     --server 127.0.0.1:9443 || echo "⚠️ Repo already exists"
 
+argocd repo add git@gitlab.gitlab.svc.cluster.local:root/inception-of-things-bonus.git \
+    --ssh-private-key-path ~/.ssh/id_rsa_argocd \
+    --insecure-ignore-host-key
+    
 echo -e "${GREEN}Initializing Git repository and pushing code...${RESET}"
 
 mkdir -p "$BONUS_DIR"
@@ -57,7 +75,9 @@ git config user.email "dabae@gmail.com"
 git config user.name "daram bae"
 
 GITLAB_USER="root"
-GITLAB_PASS=$(kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode)
+GITLAB_PASS=$(kubectl exec -n gitlab deploy/gitlab -- \
+    gitlab-rake "gitlab:password:reset[root]" 2>/dev/null | grep "Password:" | awk '{print $2}')
+# GITLAB_PASS=$(kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode)
 ENCODED_PASS=$(echo -n "$GITLAB_PASS" | curl -s -o /dev/null -w "%{url_encoded}\n" -)
 
 GITLAB_HTTP_URL="http://${GITLAB_USER}:${ENCODED_PASS}@gitlab.127.0.0.1.nip.io:8081/root/inception-of-things-bonus.git"
